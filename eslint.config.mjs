d529ce5684c04152ba303ec92b4de49803179393
Malik <mohammad.aziz@salt.co.id>
2024-09-24 10:01:04 +0700

    restructure with hapi plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import daStyle from "eslint-config-dicodingacademy";
+// import daStyle from "eslint-config-dicodingacademy";
 
 export default [
-  daStyle,
+  // daStyle,
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
--- src/server.js
+++ src/server.js
@@ -1,19 +1,26 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
-const process = require('process');
+const Hapi = require("@hapi/hapi");
+const process = require("process");
+const notes = require("./api/notes");
+const NotesService = require("./services/inMemory/NotesService");
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
