b8f846522555abbcd64f137cb2317be30ff79297
Malik <mohammad.aziz@salt.co.id>
2024-09-25 22:46:49 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ const process = require("process");
 const notes = require("./api/notes");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
 require("dotenv").config();
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,13 +21,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext("onPreResponse", (request, h) => {
     const { response } = request;
@@ -32,6 +45,7 @@ const init = async () => {
         status: "fail",
         message: response.message,
       });
+
       newResponse.code(400);
       return newResponse;
     }
